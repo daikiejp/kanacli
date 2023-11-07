118e1a38bf8d8f2377337d72dc158d792a65bdad
Danny Davila <danny@misaki.tech>
2023-11-07 01:37:45 -0500

    [Added] Shuffle function

--- bin/kanacli.ts
+++ bin/kanacli.ts
@@ -12,3 +12,11 @@ interface Kana {
   };
   message: string;
 }
+
+const shuffle = (arr: Kana[]) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
